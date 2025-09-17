5e08d7460650976e41b0457aa552afa506b0429b
Edimilson Borges <edimilson-borges159@hotmail.com>
2025-09-17 18:45:09 -0300

    feat: resolução da atividade 01

--- desafiopoomaisprati/src/main/java/com/poo/desafio/Main.java
+++ desafiopoomaisprati/src/main/java/com/poo/desafio/Main.java
@@ -2,6 +2,39 @@ package com.poo.desafio;
 
 public class Main {
     public static void main(String[] args) {
+        // Criando instância válida
+        Produto p1 = new Produto("Carro Gol", 45000.00, 5);
+        System.out.println(p1);
 
+        // Alterando valores válidos
+        p1.setPreco(50000.00);
+        p1.setQuantidadeEmEstoque(10);
+        p1.setNome("Gol 1.0");
+        System.out.println("Após alterações válidas: " + p1);
+
+        // Tentativas inválidas
+        try {
+            p1.setPreco(-50);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao definir preço: " + e.getMessage());
+        }
+
+        try {
+            p1.setQuantidadeEmEstoque(-3);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao definir quantidade: " + e.getMessage());
+        }
+
+        try {
+            p1.setNome("");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao definir nome: " + e.getMessage());
+        }
+
+        try {
+            Produto p2 = new Produto(null, 100, 5);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao criar produto: " + e.getMessage());
+        }
     }
 }
